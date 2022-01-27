23032244e552ee7089db9582856def6fb3763884
grzesiek <g.goreczny@gmail.com>
2022-01-27 02:04:52 +0100

    Initial commit

---
+++ EmployeeApi/EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+
+namespace EmployeeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly EmployeeRepository _employeeRepository = new EmployeeRepository();
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _employeeRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _employeeRepository.Get(id);
+        }
+
+        [HttpPost]
+        public bool Post(Employee employee)
+        {
+            return _employeeRepository.Add(employee);
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return _employeeRepository.Remove(id);
+        }
+    }
+}
---
+++ EmployeeApi/EmployeeApi/Models/EmployeeRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeApi.Models
+{
+    public class EmployeeRepository
+    {
+        private List<Employee> _list;
+
+        public EmployeeRepository()
+        {
+            _list = new List<Employee>() { 
+                new Employee 
+                {
+                    Id = 1,
+                    Name = "Joe Doe",
+                    City = "Berlin"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Mary Smith",
+                    City = "Paris"
+                },
+            };
+        }
+        public List<Employee> GetAll()
+        {
+            return _list;
+        }
+
+        public Employee Get( int id )
+        {
+            return _list.First( i => i.Id == id);
+        }
+
+        public bool Add(Employee newEmployee)
+        {
+            if (!_list.Any(e => e.Id == newEmployee.Id)) 
+            { 
+                _list.Add(newEmployee);
+            }
+
+            return true;
+        }
+
+        public bool Remove(int id)
+        {
+            if (_list.Any(e => e.Id == id))
+            {
+                var employee = Get(id);
+                _list.Remove(employee);
+            }
+
+            return true;
+        }
+    }
+}
